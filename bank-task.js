e9024008441b66a41ce4ec51f56c1b9ec56ead8f
rafiulislamrana <irafiul802@gmail.com>
2023-08-18 19:50:43 +0600

    deposit function added

--- bank-task.js
+++ bank-task.js
@@ -1,3 +1,30 @@
 const userName = document.getElementById('user-name').innerText = 'Rafiul Islam Rana';
 
-console.log(userName);
+// deposit js
+
+
+
+document.getElementById('add-deposit').addEventListener('click', function(){
+    const deposit_Amount = document.getElementById('deposit-amount');
+    const depositAmount = parseFloat(deposit_Amount.value);
+    const deposit_Total = document.getElementById('deposit-total').innerText;
+    const depositTotal = parseFloat(deposit_Total);
+    const balance = document.getElementById('balance').innerText;
+    const balanceTotal = parseFloat(balance)
+
+
+    const sumDeposit = depositAmount + depositTotal ;
+
+    document.getElementById('deposit-total').innerText = sumDeposit;
+
+    const sumBalance = balanceTotal + depositAmount;
+
+    document.getElementById('balance').innerText = sumBalance;
+
+    deposit_Amount.value ='';
+    
+})
+
+
+
+// withdraw js
